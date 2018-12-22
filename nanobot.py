eee8d37eced9a3c0371b74c2fcdb61b41c8190ff
vnode0 <v.wade01@gmail.com>
2018-12-22 10:27:14 -0800

    building bot

--- nanobot.py
+++ nanobot.py
@@ -1,12 +1,8 @@
-import discord
+import dscord
 from discord.ext import commands
 import os
 import json
 import asyncio
-
-# Authentication
-TOKEN = 'MjY4OTQ3ODc3NTU0ODE0OTg2.Dv_vgQ.eqi8NTGOj5wtvlWcELATt4RLlqU'
-
 
 # Configuration Setup
 bot = commands.Bot(command_prefix = '.')
@@ -18,10 +14,11 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
+	logname = (message.server.name + '[' + hash(message.server) + ']', 'DM_' + message.author, 'DM_' + message.author )[message.server != None]
 	author = message.author
 	body = message.content
-	with open('messages.txt', 'a+') as f:
+	with open( logname + 'log.txt', 'a+') as f:
 		log = author + body
 		f.write(message.content)
 
-bot.run(TOKEN)
+bot.run(os.environ['NANOBOT_TOKEN'])
