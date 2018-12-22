5a8e062ca9932697980385d7e1ec9e8bf3703071
vnode0 <v.wade01@gmail.com>
2018-12-22 08:08:57 -0800

    building bot

--- nanobot.py
+++ nanobot.py
@@ -1,6 +1,27 @@
 import discord
+from discord.ext import commands
 import os
 import json
 import asyncio
 
-bot = discord.Client()
+# Authentication
+TOKEN = 'MjY4OTQ3ODc3NTU0ODE0OTg2.Dv_vgQ.eqi8NTGOj5wtvlWcELATt4RLlqU'
+
+
+# Configuration Setup
+bot = commands.Bot(command_prefix = '.')
+client = discord.Client()
+
+@bot.event
+async def on_ready():
+	print('nanobot online.')
+
+@bot.event
+async def on_message(message):
+	author = message.author
+	body = message.content
+	with open('messages.txt', 'a+') as f:
+		log = author + body
+		f.write(message.content)
+
+bot.run(TOKEN)
